29fe0e23c4721587bc910fa2c2686af0515c9bac
kaduvibraga <cevb@cin.ufpe.br>
2020-11-26 20:52:35 -0300

    JS

---
+++ JavaScript/main.js
@@ -0,0 +1,5 @@
+var nome = "Kadu Braga";
+var idade = 23;
+var frase = "Brasil é o pipoco";
+alert ("meu primeiro js! " + nome + " tem " + idade + " anos.");
+alert (frase.replace ("Brasil", "inglaterra"));
