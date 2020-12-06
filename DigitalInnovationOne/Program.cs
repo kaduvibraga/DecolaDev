2f64b0005f52c3f1842c72394fd9077f259fc912
kaduvibraga <cevb@cin.ufpe.br>
2020-12-05 23:01:00 -0300

    dotnet

---
+++ DigitalInnovationOne/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DigitalInnovationOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numerodevezes = 5;
+
+            for (int i = 0; i < numerodevezes; i++)
+            {
+                Console.WriteLine($"Bem vindo ao dotnet {i}");
+
+            }
+        }
+    }
+}
--- JavaScript/main.js
+++ JavaScript/main.js
@@ -3,3 +3,8 @@ var idade = 23;
 var frase = "Brasil é o pipoco";
 alert ("meu primeiro js! " + nome + " tem " + idade + " anos.");
 alert (frase.replace ("Brasil", "inglaterra"));
+
+var lista= ["ovo", "salsicha", "miojo"];
+lista.push("hamburguer");
+
+console.log (lista);
