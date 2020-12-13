76bc32d8d9afe40ad48c2471e0c48e46061707f7
kaduvibraga <cevb@cin.ufpe.br>
2020-12-13 14:02:33 -0300

    Asp Net

---
+++ ASP net/ASPNET/ASPNET/Models/Context.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET.Models
+{
+    public class Context : DbContext 
+    {
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(connectionString:@"Server=(localdb)\mssqllocaldb;Database=ASPNET;Integrated Security=True");
+        }
+
+
+    }
+}
